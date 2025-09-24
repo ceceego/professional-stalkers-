17eca5317b21cabe659132f2922f81fcfcf89e87
charleyblue27 <charblue01@gmail.com>
2025-09-24 09:49:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-
 const checkin_button = document.getElementById('checkin-btn');
 const checkout_button = document.getElementById('checkout-btn')
 const statustext = document.querySelector('.status-text');
 
 checkin_button.addEventListener('click', function() {
     statustext.textContent = 'Currently Checked In'
+    status_text.style.color = 'green'
 });
 
 checkout_button.addEventListener('click', function() {
     statustext.textContent = 'Currently Checked Out'
+    status_text.style.color = 'red'
 });
