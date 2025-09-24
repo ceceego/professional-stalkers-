63a60fb2cb5dbc3eaed168a29bcb2b75915a7268
charleyblue27 <charblue01@gmail.com>
2025-09-24 18:50:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const checkin_button = document.getElementById('checkin-btn');
 const checkout_button = document.getElementById('checkout-btn')
 const statustext = document.querySelector('.status-text');
+const indicatorstatus = document.querySelector('.indicator');
 
 checkin_button.addEventListener('click', function() {
     statustext.textContent = 'Currently Checked In'
-    statustext.style.color = 'green'
+    statustext.style.color = '#2ecc71'
+    indicatorstatus.style.backgroundColor = '#2ecc71'
 });
 
 checkout_button.addEventListener('click', function() {
     statustext.textContent = 'Currently Checked Out'
-    statustext.style.color = 'red'
+    statustext.style.color = '#A41D36'
+    indicatorstatus.style.backgroundColor = '#A41D36'
 });
-
