55fa6591287ee3f29f527784fab42e507c09b5cf
T.J. Seislove <tseislove@muhlenberg.edu>
2025-09-26 13:24:38 -0400

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,9 @@
+export const login = async (req, res) => {
+  // TODO: Check user credentials against DB
+  res.json({ message: "Login endpoint" });
+};
 
+export const logout = async (req, res) => {
+  // TODO: Handle logout/session cleanup
+  res.json({ message: "Logout endpoint" });
+};
