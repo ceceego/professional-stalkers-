6a216ed8850578b4336f92f15e1b3999cd53cd4b
T.J. Seislove <tseislove@muhlenberg.edu>
2025-10-06 12:39:13 -0400

    Add files via upload

---
+++ frontend/registerView/register_script.js
@@ -0,0 +1,18 @@
+
+
+document.querySelector("#loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+    const firstname = document.getElementById("firstname").value
+    const lastname = document.getElementById("lastname").value
+    const username = document.getElementById("username").value
+    const usertype = document.getElementById("usertype").value
+    const password = document.getElementById("password").value
+
+    if (usertype === "faculty"){
+        window.location.href = "index.html";
+    }
+    else if (usertype === "student"){
+        window.location.href = "index.html"; // replace this with student dashboard when done
+    }
+});
