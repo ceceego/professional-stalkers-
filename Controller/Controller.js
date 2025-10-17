92b9fb1af98e9d15c7453ca5e6ad3504bbd6e740
celia <ceceforever6617@gmail.com>
2025-10-17 13:47:27 -0400

    Update and rename Controller.js to OfficeHoursController

--- Controller/Controller.js
+++
@@ -1,9 +0,0 @@
-export const login = async (req, res) => {
-  // TODO: Check user credentials against DB
-  res.json({ message: "Login endpoint" });
-};
-
-export const logout = async (req, res) => {
-  // TODO: Handle logout/session cleanup
-  res.json({ message: "Logout endpoint" });
-};
