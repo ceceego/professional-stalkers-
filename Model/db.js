de0d8d9e7945dde54e7dd6f8e3fc04e9a340f0ef
T.J. Seislove <tseislove@muhlenberg.edu>
2025-10-20 12:53:15 -0400

    Update db.js

--- Model/db.js
+++ Model/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2/promise";
 export const db = mysql.createPool({
   host: "172.19.120.201",
   user: "root",
-  password: "your_password",
+  password: "password",
   database: "office_hours"
 });
