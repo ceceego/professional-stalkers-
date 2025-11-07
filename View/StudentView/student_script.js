58b6087da02ef942bf2960097211e93ef0b025fb
T.J. Seislove <tseislove@muhlenberg.edu>
2025-11-07 14:10:55 -0500

    Update student_script.js

--- View/StudentView/student_script.js
+++ View/StudentView/student_script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const favorite_star = document.querySelector('.favorite-btn');
 
 favorite_star.addEventListener('click', function () {
@@ -12,20 +11,3 @@ favorite_star.addEventListener('click', function () {
         favorite_star.style.color = gold;
     }
 });
-
-=======
-const favorite_star = document.querySelector('.favorite-btn');
-
-favorite_star.addEventListener('click', function () {
-    const gold = 'gold';
-    const gray = '#A4A9AD';
-
-    const currentColor = favorite_star.style.color;
-    if (currentColor == 'gold') {
-        favorite_star.style.color = gray;
-    } else {
-        favorite_star.style.color = gold;
-    }
-});
-
->>>>>>> fb799d1c8db0db472e26fbbf1062cd4368989ec6
